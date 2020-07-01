a538f67888bc09a7d3a0c21ff7faeeb7d17ab7e2
marwai <mtse@spartaglobal.com>
2020-07-01 15:41:34 +0100

    For Loops

--- devops_training/week_3_python_week/109_loops.py
+++ devops_training/week_3_python_week/109_loops.py
@@ -1,7 +1,16 @@
 # Loops
 
 # For Loops are used to iterate through lists, strings Dictionaries and Tuples
-# WHILE loops runs if True
+# While loops runs if True
 
-list_data = [1, 2, 3]
-print(list_data)
+# Syntax: For loop
+# iterate through a whole list in ms
+list_data = [1, 2, 3, 4, 5, 6]
+# print(list_data[0])
+# print(list_data[1])
+# print(list_data[2])
+
+for data in list_data:
+    if data > 5:
+        break
+    print(data)
