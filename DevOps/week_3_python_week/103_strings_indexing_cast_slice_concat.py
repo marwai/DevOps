afe51d8e1d8f7b1074d647cc5203e04a8b46ba15
marwai <mtse@spartaglobal.com>
2020-07-01 11:11:34 +0100

    strings

--- DevOps/week_3_python_week/103_strings_indexing_cast_slice_concat.py
+++ DevOps/week_3_python_week/103_strings_indexing_cast_slice_concat.py
@@ -1,17 +1,42 @@
 # strings - casting - slicing  - concatenation
 
+# Concatenation
 # greeting_welcome = "Hello World"
-welcome_user = input("Please enter your name? ")
-print("Dear " + welcome_user + " welcome on board") #Concatenation
+# welcome_user = input("Please enter your name? ")
+# print("Dear " + welcome_user + " welcome on board") #Concatenation
 # print(greeting_welcome)
-#  H E L L O W O R L D
-# 0 1 2 3 4 5 6 7 8 9
+# HELLO WORLD
+# 012345678910
 # print(len(greeting_welcome))
-print("Hi {}, welcome to Python".format(welcome_user))
+# print("Hi {}, welcome to Python".format(welcome_user))
 
 # indexing
 # String slicing
+# hi = "Hello world"
+# print(len(hi)) # prints length of string
+# print(hi[0]) # print h because it starts on 0
+# print(hi[-1]) 0 prints d
+#
+# print(hi[0:6]) print hello
+# print(hi[6:11]) print world
+# print(hi[1:3])
 
-hi = "Hello world"
-print(hi[0])
-print(hi[])
+# Slicing
+# remove_white_space = "Remove the space at the end of a string        "
+# print(len(remove_white_space)
+# print(len(remove_white_space.strip()))
+# print(len(remove_white_space))
+
+# .count() counts the substring within the string
+use_text = "Here's SOME text with lot's of text"
+print(use_text.count("text"))
+# .lower() lowers the caps within the string
+print(use_text.lower())
+
+# .upper() makes the first letters uppercase in each word
+print(use_text.upper())
+
+# .title makes everything UPPERCASE
+print(use_text.title())
+
+
